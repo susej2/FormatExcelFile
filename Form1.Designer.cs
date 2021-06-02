3981e467f38a8bc3a5eea49f44eadf6df5782d1b
susej2 <35945119+susej2@users.noreply.github.com>
2021-06-01 21:04:14 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace WorkExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.txt_sheet = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_to_lower = new System.Windows.Forms.Button();
+            this.txt_export = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_to_upper = new System.Windows.Forms.Button();
+            this.btn_email = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(526, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(122, 29);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(366, 20);
+            this.txt_path.TabIndex = 2;
+            // 
+            // txt_sheet
+            // 
+            this.txt_sheet.Location = new System.Drawing.Point(122, 69);
+            this.txt_sheet.Name = "txt_sheet";
+            this.txt_sheet.Size = new System.Drawing.Size(366, 20);
+            this.txt_sheet.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(752, 409);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btn_to_lower
+            // 
+            this.btn_to_lower.Location = new System.Drawing.Point(612, 26);
+            this.btn_to_lower.Name = "btn_to_lower";
+            this.btn_to_lower.Size = new System.Drawing.Size(80, 25);
+            this.btn_to_lower.TabIndex = 5;
+            this.btn_to_lower.Text = "To Lower";
+            this.btn_to_lower.UseVisualStyleBackColor = true;
+            this.btn_to_lower.Click += new System.EventHandler(this.txt_format_Click);
+            // 
+            // txt_export
+            // 
+            this.txt_export.Location = new System.Drawing.Point(526, 27);
+            this.txt_export.Name = "txt_export";
+            this.txt_export.Size = new System.Drawing.Size(80, 24);
+            this.txt_export.TabIndex = 6;
+            this.txt_export.Text = "Export";
+            this.txt_export.UseVisualStyleBackColor = true;
+            this.txt_export.Click += new System.EventHandler(this.txt_export_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Type sheet name";
+            // 
+            // btn_to_upper
+            // 
+            this.btn_to_upper.Location = new System.Drawing.Point(612, 66);
+            this.btn_to_upper.Name = "btn_to_upper";
+            this.btn_to_upper.Size = new System.Drawing.Size(80, 25);
+            this.btn_to_upper.TabIndex = 8;
+            this.btn_to_upper.Text = "To Upper";
+            this.btn_to_upper.UseVisualStyleBackColor = true;
+            this.btn_to_upper.Click += new System.EventHandler(this.btn_to_upper_Click);
+            // 
+            // btn_email
+            // 
+            this.btn_email.Location = new System.Drawing.Point(698, 26);
+            this.btn_email.Name = "btn_email";
+            this.btn_email.Size = new System.Drawing.Size(80, 25);
+            this.btn_email.TabIndex = 9;
+            this.btn_email.Text = "Fix Email";
+            this.btn_email.UseVisualStyleBackColor = true;
+            this.btn_email.Click += new System.EventHandler(this.btn_email_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 517);
+            this.Controls.Add(this.btn_email);
+            this.Controls.Add(this.btn_to_upper);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_export);
+            this.Controls.Add(this.btn_to_lower);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_sheet);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Transform";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.TextBox txt_sheet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_to_lower;
+        private System.Windows.Forms.Button txt_export;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_to_upper;
+        private System.Windows.Forms.Button btn_email;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Select file";
+            openFileDialog.InitialDirectory = @"c:\";           
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+           
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txt_path.Text = openFileDialog.FileName;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string filePath = txt_path.Text;
+            string extension = Path.GetExtension(filePath);
+            string conStr="";
+
+            switch (extension)
+            {
+                case ".xls": //Excel 97-03
+                    conStr = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'", filePath, "YES");
+                    break;
+
+                case ".xlsx": //Excel 07
+                    conStr = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'", filePath, "YES");
+                    break;
+            }
+
+            OleDbConnection conn = new OleDbConnection(conStr);
+
+            OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter("Select * from ["+txt_sheet.Text+"$]", conn);
+            DataTable dataTable = new DataTable();
+
+            oleDbDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.Programmatic;
+            }
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullColumnSelect;
+        }
+
+        private void txt_format_Click(object sender, EventArgs e)
+        {
+            Int32 selectedColumnCount = dataGridView1.Columns
+        .GetColumnCount(DataGridViewElementStates.Selected);
+
+            if (selectedColumnCount > 0)
+            {
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    if (cell.Value == null)
+                    {
+                        break;
+                    }
+                  cell.Value= cell.Value.ToString().ToLower();
+                } 
+            }
+        }
+
+        private void btn_to_upper_Click(object sender, EventArgs e)
+        {
+            Int32 selectedColumnCount = dataGridView1.Columns
+                  .GetColumnCount(DataGridViewElementStates.Selected);
+
+            if (selectedColumnCount > 0)
+            {
+
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    if (cell.Value == null)
+                    {
+                        break;
+                    }
+                    cell.Value = cell.Value.ToString().ToUpper();
+                }
+
+            }
+        }
+
+        private void btn_email_Click(object sender, EventArgs e)
+        {
+            Int32 selectedColumnCount = dataGridView1.Columns
+                 .GetColumnCount(DataGridViewElementStates.Selected);
+            List<char> chars;
+            List<char> charsM = new List<char>();
+            string value="";
+
+            if (selectedColumnCount > 0)
+            {
+
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    if (cell.Value == null)
+                    {
+                        break;
+                    }
+
+                    chars = cell.Value.ToString().ToList();
+                    charsM = cell.Value.ToString().ToList();
+
+                    foreach (var item in chars)
+                    {
+                        if (item == ' ')
+                        {
+                            charsM.Remove(item);
+                        }
+                    }
+
+                    value = "";
+
+                    foreach (var item in charsM)
+                    {
+                        value += item;
+                    }
+
+                    cell.Value = value;
+
+                }
+
+            }
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void txt_export_Click(object sender, EventArgs e)
+        {
+            copyAlltoClipboard();
+            Microsoft.Office.Interop.Excel.Application xlexcel;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlexcel = new Microsoft.Office.Interop.Excel.Application();
+            xlexcel.Visible = true;
+            xlWorkBook = xlexcel.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
+            CR.Select();
+            xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+        }
+    }
+}
